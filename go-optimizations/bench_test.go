3f51979e5379d5fd76c717d04f0fb8300919bc3e
Denis <d.limarev@corp.mail.ru>
2021-11-03 01:51:46 +0300

    add new meetup

---
+++ go-optimizations/bench_test.go
@@ -0,0 +1,15 @@
+package go_optimizations
+
+import "testing"
+
+func BenchmarkRangeValue(b *testing.B) {
+
+}
+
+func BenchmarkRangeArray(b *testing.B) {
+
+}
+
+func BenchmarkFmt(b *testing.B) {
+
+}
