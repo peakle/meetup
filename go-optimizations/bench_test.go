1ee3da793f8ca70f911b82cbbbdbee633eafd663
Denis <d.limarev@corp.mail.ru>
2021-11-05 19:35:44 +0300

    add benchmarks

--- go-optimizations/bench_test.go
+++ go-optimizations/bench_test.go
@@ -1,13 +1,17 @@
 package go_optimizations
 
 import (
+	"sync"
 	"testing"
 )
 
 const benchCount = 1000000
 
 const (
-	smallArraySize = 1024 << (1 * iota)
+	extraSmallArraySize = 64 << (1 * iota)
+	smallArraySize
+	_
+	_
 	mediumArraySize
 	hugeArraySize
 )
@@ -76,13 +80,13 @@ func BenchmarkRangeArrayWithPointer(b *testing.B) {
 }
 
 func BenchmarkMakeIncorrectUsage(b *testing.B) {
-	var t = make([][smallArraySize]byte, 10)
+	var t = make([][extraSmallArraySize]byte, 10)
 
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		b.Run("benchmark_make_uncorrect_usage", func(b *testing.B) {
+		b.Run("benchmark_make_incorrect_usage", func(b *testing.B) {
 			for ii := 0; ii < benchCount; ii++ {
-				t = append(t, [smallArraySize]byte{})
+				t = append(t, [extraSmallArraySize]byte{})
 			}
 		})
 	}
@@ -90,15 +94,147 @@ func BenchmarkMakeIncorrectUsage(b *testing.B) {
 }
 
 func BenchmarkMakeCorrectUsage(b *testing.B) {
-	var t = make([][smallArraySize]byte, 0, benchCount)
+	var t = make([][extraSmallArraySize]byte, 0, benchCount)
 
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		b.Run("benchmark_make_correct_usage", func(b *testing.B) {
 			for ii := 0; ii < benchCount; ii++ {
-				t = append(t, [smallArraySize]byte{})
+				t = append(t, [extraSmallArraySize]byte{})
 			}
 		})
 	}
 	b.StopTimer()
 }
+
+type hugeStruct struct {
+	h     int
+	cache [hugeArraySize]byte
+}
+
+func BenchmarkHugeParamByCopy(b *testing.B) {
+	t := hugeStruct{
+		h:     0,
+		cache: [2048]byte{},
+	}
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		b.Run("benchmark_huge_param_by_copy", func(b *testing.B) {
+			for ii := 0; ii < benchCount; ii++ {
+				t = dummyCopy(t)
+			}
+		})
+	}
+	b.StopTimer()
+}
+
+func dummyCopy(h hugeStruct) hugeStruct {
+	for i := 0; i < 10; i++ {
+		h.h = i
+	}
+
+	return h
+}
+
+func BenchmarkHugeParamByPointer(b *testing.B) {
+	t := &hugeStruct{
+		h:     0,
+		cache: [2048]byte{},
+	}
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		b.Run("benchmark_huge_param_by_pointer", func(b *testing.B) {
+			for ii := 0; ii < benchCount; ii++ {
+				t = dummyPointer(t)
+			}
+		})
+	}
+	b.StopTimer()
+}
+
+func dummyPointer(h *hugeStruct) *hugeStruct {
+	for i := 0; i < 10; i++ {
+		h.h = i
+	}
+
+	return h
+}
+
+func BenchmarkNewObject(b *testing.B) {
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		b.Run("benchmark_new_object", func(b *testing.B) {
+			var wg sync.WaitGroup
+			wg.Add(benchCount)
+
+			for ii := 0; ii < benchCount; ii++ {
+				go func() {
+					h := &hugeStruct{h: 0, cache: [2048]byte{}}
+					h = dummyPointer(h)
+					wg.Done()
+				}()
+			}
+
+			wg.Wait()
+		})
+	}
+	b.StopTimer()
+}
+
+var hugeStructPool sync.Pool
+
+func BenchmarkNewObjectWithSyncPool(b *testing.B) {
+	get := func() *hugeStruct {
+		h := hugeStructPool.Get()
+		if h == nil {
+			return &hugeStruct{}
+		}
+		return h.(*hugeStruct)
+	}
+	put := func(h *hugeStruct) {
+		h.h = 0
+		hugeStructPool.Put(h)
+	}
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		b.Run("benchmark_new_object", func(b *testing.B) {
+			var wg sync.WaitGroup
+			wg.Add(benchCount)
+
+			for ii := 0; ii < benchCount; ii++ {
+				go func() {
+					h := get()
+					h = dummyPointer(h)
+					wg.Done()
+
+					put(h)
+				}()
+			}
+
+			wg.Wait()
+		})
+	}
+	b.StopTimer()
+}
+
+func BenchmarkSlice(b *testing.B) {
+
+}
+
+func BenchmarkSliceReuse(b *testing.B) {
+
+}
+
+func BenchmarkRawGoroutines(b *testing.B)       {}
+func BenchmarkSemaphoreGoroutines(b *testing.B) {}
+func BenchmarkReusableGoroutines(b *testing.B)  {}
+func BenchmarkGC(b *testing.B)                  {}
+func BenchmarkGCWithBallast(b *testing.B)       {}
+func BenchmarkStrings(b *testing.B)             {}
+func BenchmarkBytes(b *testing.B)               {}
+func BenchmarkFmt(b *testing.B)                 {}
+func BenchmarkConcatenation(b *testing.B)       {}
+func BenchmarkStructSizes(b *testing.B)         {}
