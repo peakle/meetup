829c00b985b19ed0542d4c973d368ab3a57ffef1
Denis <d.limarev@corp.mail.ru>
2021-11-05 09:26:09 +0300

    add make tests

--- go-optimizations/bench_test.go
+++ go-optimizations/bench_test.go
@@ -5,15 +5,20 @@ import (
 )
 
 const benchCount = 1000000
-const arraySize = 16384
 
-var byteArray [arraySize]byte
-var byteSlice [][arraySize]byte
+const (
+	smallArraySize = 1024 << (1 * iota)
+	mediumArraySize
+	hugeArraySize
+)
+
+var byteArray [hugeArraySize]byte
+var byteSlice [][hugeArraySize]byte
 
 func BenchmarkRangeValueCopy(b *testing.B) {
-	var t [arraySize]byte
+	var t [hugeArraySize]byte
 	b.StartTimer()
-	for i := 0; i > b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		b.Run("range_value_copy", func(b *testing.B) {
 			for _, str := range byteSlice {
 				t = str
@@ -26,9 +31,9 @@ func BenchmarkRangeValueCopy(b *testing.B) {
 }
 
 func BenchmarkRangeValueIndex(b *testing.B) {
-	var t *[arraySize]byte
+	var t *[hugeArraySize]byte
 	b.StartTimer()
-	for i := 0; i > b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		b.Run("range_value_index", func(b *testing.B) {
 			for ii := range byteSlice {
 				t = &byteSlice[ii]
@@ -43,7 +48,7 @@ func BenchmarkRangeValueIndex(b *testing.B) {
 func BenchmarkRangeArrayValue(b *testing.B) {
 	var t byte
 	b.StartTimer()
-	for i := 0; i > b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		b.Run("range_array", func(b *testing.B) {
 			for _, v := range byteArray {
 				t = v
@@ -58,7 +63,7 @@ func BenchmarkRangeArrayValue(b *testing.B) {
 func BenchmarkRangeArrayWithPointer(b *testing.B) {
 	var t byte
 	b.StartTimer()
-	for i := 0; i > b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		b.Run("range_array_with_pointer", func(b *testing.B) {
 			for _, v := range &byteArray {
 				t = v
@@ -70,6 +75,30 @@ func BenchmarkRangeArrayWithPointer(b *testing.B) {
 	_ = t
 }
 
-func BenchmarkFmt(b *testing.B) {
+func BenchmarkMakeIncorrectUsage(b *testing.B) {
+	var t = make([][smallArraySize]byte, 10)
 
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		b.Run("benchmark_make_uncorrect_usage", func(b *testing.B) {
+			for ii := 0; ii < benchCount; ii++ {
+				t = append(t, [smallArraySize]byte{})
+			}
+		})
+	}
+	b.StopTimer()
+}
+
+func BenchmarkMakeCorrectUsage(b *testing.B) {
+	var t = make([][smallArraySize]byte, 0, benchCount)
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		b.Run("benchmark_make_correct_usage", func(b *testing.B) {
+			for ii := 0; ii < benchCount; ii++ {
+				t = append(t, [smallArraySize]byte{})
+			}
+		})
+	}
+	b.StopTimer()
 }
