1f16a7aadb5f7d7d5477824590cbdcfa5b27a6a9
Denis <d.limarev@corp.mail.ru>
2021-11-21 17:16:52 +0300

    fix benchmarks

--- go-optimizations/bench_test.go
+++ go-optimizations/bench_test.go
@@ -80,31 +80,39 @@ func BenchmarkRangeValueCopy(b *testing.B) {
 	var hugeSlice = make([]hugeStruct, benchCount)
 
 	b.Run("range_value_copy", func(b *testing.B) {
-		for _, hs := range hugeSlice {
-			sum += hs.h
+		for i := 0; i < b.N; i++ {
+			for _, hs := range hugeSlice {
+				sum += hs.h
+			}
 		}
 	})
 
 	b.Run("range_value_index", func(b *testing.B) {
-		for ii := range hugeSlice {
-			sum += hugeSlice[ii].h
+		for i := 0; i < b.N; i++ {
+			for ii := range hugeSlice {
+				sum += hugeSlice[ii].h
+			}
 		}
 	})
 
 	b.Run("range_value_pointer_and_index", func(b *testing.B) {
-		for ii := range hugeSlice {
-			sum += (&hugeSlice[ii]).h
+		for i := 0; i < b.N; i++ {
+			for ii := range hugeSlice {
+				sum += (&hugeSlice[ii]).h
+			}
 		}
 	})
 
 	b.Run("range_value_pointer_and_index_split", func(b *testing.B) {
-		for ii := range hugeSlice {
-			h := &hugeSlice[ii]
-			sum += h.h
+		for i := 0; i < b.N; i++ {
+			for ii := range hugeSlice {
+				h := &hugeSlice[ii]
+				sum += h.h
+			}
 		}
 	})
 
-	_ = sum
+	b.Logf("sum: %d", sum)
 }
 
 func BenchmarkRangeArrayValue(b *testing.B) {
@@ -114,8 +122,10 @@ func BenchmarkRangeArrayValue(b *testing.B) {
 	b.StartTimer()
 
 	b.Run("range_array", func(b *testing.B) {
-		for _, v := range hugeArray {
-			sum += v.h
+		for i := 0; i < b.N; i++ {
+			for _, v := range hugeArray {
+				sum += v.h
+			}
 		}
 	})
 
@@ -129,8 +139,10 @@ func BenchmarkRangeArrayWithPointer(b *testing.B) {
 	b.StartTimer()
 
 	b.Run("range_array_with_pointer", func(b *testing.B) {
-		for _, v := range &hugeArray {
-			sum += v.h
+		for i := 0; i < b.N; i++ {
+			for _, v := range &hugeArray {
+				sum += v.h
+			}
 		}
 	})
 
@@ -141,7 +153,7 @@ func BenchmarkMakeIncorrectUsage(b *testing.B) {
 	b.Run("benchmark_make_incorrect_usage", func(b *testing.B) {
 		var t = make([][extraSmallArraySize]byte, 0, 10)
 
-		for ii := 0; ii < benchCount; ii++ {
+		for ii := 0; ii < b.N; ii++ {
 			t = append(t, [extraSmallArraySize]byte{})
 		}
 	})
@@ -149,9 +161,9 @@ func BenchmarkMakeIncorrectUsage(b *testing.B) {
 
 func BenchmarkMakeCorrectUsage(b *testing.B) {
 	b.Run("benchmark_make_correct_usage", func(b *testing.B) {
-		var t = make([][extraSmallArraySize]byte, 0, benchCount)
+		var t = make([][extraSmallArraySize]byte, 0, b.N)
 
-		for ii := 0; ii < benchCount; ii++ {
+		for ii := 0; ii < b.N; ii++ {
 			t = append(t, [extraSmallArraySize]byte{})
 		}
 	})
@@ -167,7 +179,7 @@ func BenchmarkHugeParamByCopy(b *testing.B) {
 	b.StartTimer()
 
 	b.Run("benchmark_huge_param_by_copy", func(b *testing.B) {
-		for ii := 0; ii < benchCount; ii++ {
+		for ii := 0; ii < b.N; ii++ {
 			t = dummyCopy(t)
 		}
 	})
@@ -190,7 +202,7 @@ func BenchmarkHugeParamByPointer(b *testing.B) {
 
 	b.StartTimer()
 	b.Run("benchmark_huge_param_by_pointer", func(b *testing.B) {
-		for ii := 0; ii < benchCount; ii++ {
+		for ii := 0; ii < b.N; ii++ {
 			t = dummyPointer(t)
 		}
 	})
@@ -214,9 +226,9 @@ func BenchmarkNewObject(b *testing.B) {
 			wg sync.WaitGroup
 			h  *hugeStruct
 		)
-		wg.Add(benchCount)
+		wg.Add(b.N)
 
-		for ii := 0; ii < benchCount; ii++ {
+		for ii := 0; ii < b.N; ii++ {
 			go func() {
 				h = &hugeStruct{body: make([]byte, 0, mediumArraySize)}
 				h = dummyPointer(h)
@@ -254,9 +266,9 @@ func BenchmarkNewObjectWithSyncPool(b *testing.B) {
 	b.StartTimer()
 	b.Run("new_object_with_sync_pool", func(b *testing.B) {
 		var wg sync.WaitGroup
-		wg.Add(benchCount)
+		wg.Add(b.N)
 
-		for ii := 0; ii < benchCount; ii++ {
+		for ii := 0; ii < b.N; ii++ {
 			go func() {
 				h = get()
 				h = dummyPointer(h)
@@ -275,12 +287,18 @@ func BenchmarkNewObjectWithSyncPool(b *testing.B) {
 	b.Logf("counter: %d", h.h)
 }
 
+var mu sync.Mutex
+
 func dummyProcess() int64 {
 	var sum int64
 	for i := int64(0); i < 1000; i++ {
 		sum += i
 	}
-	//time.Sleep(time.Microsecond)
+
+	mu.Lock()
+	sum += 2
+	mu.Unlock()
+
 	return sum
 }
 
@@ -297,8 +315,8 @@ func BenchmarkGoroutinesRaw(b *testing.B) {
 	b.StartTimer()
 
 	b.Run("raw_goroutines", func(t *testing.B) {
-		wg.Add(benchCount)
-		for j := 0; j < benchCount; j++ {
+		wg.Add(b.N)
+		for j := 0; j < b.N; j++ {
 			go process()
 		}
 		wg.Wait()
@@ -320,8 +338,8 @@ func BenchmarkGoroutinesRawNotOptimized(b *testing.B) {
 	b.StartTimer()
 
 	b.Run("raw_goroutines_not_optimized", func(t *testing.B) {
-		wg.Add(benchCount)
-		for j := 0; j < benchCount; j++ {
+		wg.Add(b.N)
+		for j := 0; j < b.N; j++ {
 			go func() {
 				atomic.AddInt64(&counter, dummyProcess())
 				wg.Done()
@@ -329,7 +347,6 @@ func BenchmarkGoroutinesRawNotOptimized(b *testing.B) {
 		}
 		wg.Wait()
 	})
-	b.StopTimer()
 
 	stats := checkMem()
 	b.Logf("memory usage:%d MB", stats.TotalAlloc/MiB)
@@ -352,8 +369,8 @@ func BenchmarkGoroutinesSemaphore(b *testing.B) {
 	b.StartTimer()
 
 	b.Run("semaphore", func(t *testing.B) {
-		wg.Add(benchCount)
-		for j := 0; j < benchCount; j++ {
+		wg.Add(b.N)
+		for j := 0; j < b.N; j++ {
 			sema <- struct{}{}
 			go process()
 		}
@@ -385,8 +402,8 @@ func BenchmarkReusableGoroutines(b *testing.B) {
 	b.StartTimer()
 
 	b.Run("reusable_goroutines", func(b *testing.B) {
-		wg.Add(benchCount)
-		for j := 0; j < benchCount; j++ {
+		wg.Add(b.N)
+		for j := 0; j < b.N; j++ {
 			p.Submit(nil)
 		}
 		wg.Wait()
@@ -402,21 +419,21 @@ func BenchmarkReusableGoroutines(b *testing.B) {
 func BenchmarkGC(b *testing.B) {
 	b.StopTimer()
 	var wg sync.WaitGroup
-	wg.Add(1)
 	b.StartTimer()
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 100; i++ {
-			dummyApplication(benchCount / 2)
+	b.Run("gc_without_ballast", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				dummyApplication(benchCount / 2)
+				time.Sleep(time.Nanosecond)
+			}()
+			dummyApplication(benchCount)
 			time.Sleep(time.Nanosecond)
+			wg.Wait()
 		}
-	}()
-	for i := 0; i < 10; i++ {
-		dummyApplication(benchCount)
-		time.Sleep(time.Nanosecond)
-	}
-	wg.Wait()
+	})
 	b.StopTimer()
 
 	stats := checkMem()
@@ -433,21 +450,21 @@ func BenchmarkGCWithBallast(b *testing.B) {
 		ballast = make([]byte, 10<<30)
 		wg      sync.WaitGroup
 	)
-	wg.Add(1)
 	b.StartTimer()
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 100; i++ {
-			dummyApplication(benchCount / 2)
+	b.Run("gc_with_ballast", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				dummyApplication(benchCount / 2)
+				time.Sleep(time.Nanosecond)
+			}()
+			dummyApplication(benchCount)
 			time.Sleep(time.Nanosecond)
+			wg.Wait()
 		}
-	}()
-	for i := 0; i < 10; i++ {
-		dummyApplication(benchCount)
-		time.Sleep(time.Nanosecond)
-	}
-	wg.Wait()
+	})
 	b.StopTimer()
 
 	func(_ []byte) {
@@ -459,6 +476,7 @@ func BenchmarkGCWithBallast(b *testing.B) {
 	stats := checkMem()
 	b.Logf("memory usage: %d MB", stats.TotalAlloc/MiB)
 	b.Logf("GC cycles: %d", stats.NumGC)
+	b.Logf("%d", ballast[2])
 }
 
 //bench_test.go:353: memory usage: 69813 MB
@@ -478,35 +496,32 @@ func dummyApplication(count int) {
 	wg.Wait()
 }
 
-func BenchmarkStrings(b *testing.B) {}
-func BenchmarkBytes(b *testing.B)   {}
-
 func BenchmarkInterfaceUsage(b *testing.B) {
 	b.StopTimer()
 	var foo string
 	b.StartTimer()
 
 	b.Run("fmt_sprint", func(b *testing.B) {
-		for i := 0; i < benchCount; i++ {
+		for i := 0; i < b.N; i++ {
 			foo = fmt.Sprint("foo", "bar")
 		}
 	})
 
 	b.Run("fmt_sprintf", func(b *testing.B) {
-		for i := 0; i < benchCount; i++ {
+		for i := 0; i < b.N; i++ {
 			foo = fmt.Sprintf("foo %s", "bar")
 		}
 	})
 
 	// TODO why slower than fmt.Sprint
 	b.Run("fmt_sprint_string", func(b *testing.B) {
-		for i := 0; i < benchCount; i++ {
+		for i := 0; i < b.N; i++ {
 			foo = customFmt.SprintString("foo", "bar")
 		}
 	})
 
 	b.Run("concatenation", func(b *testing.B) {
-		for i := 0; i < benchCount; i++ {
+		for i := 0; i < b.N; i++ {
 			foo = "foo" + "bar"
 		}
 	})
