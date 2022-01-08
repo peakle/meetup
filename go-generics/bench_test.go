01aab1248f62388f5917f9ba450a48216fc7d84c
Denis <denislimarev@gmail.com>
2022-01-08 15:19:30 +0300

    add benchmarks for generics

--- go-generics/bench_test.go
+++ go-generics/bench_test.go
@@ -1,87 +1,134 @@
 package generics
 
 import (
-	"fmt"
 	"runtime"
-	"sync"
-	"sync/atomic"
 	"testing"
 )
 
-// runner - run batched func for multiply goroutines
-func runner(b *testing.B, name string, ng int, procFunc func(i int64) int64) bool {
-	return b.Run(fmt.Sprintf("type:%s-goroutines:%d", name, ng), func(b *testing.B) {
-		var wg sync.WaitGroup
-		var trigger int64 = 0
-		n := b.N
-		// if we will get batchSize = 1000 and n = 100k
-		// we will start 1000 goroutines, each of which will execute 100 operations
-		batchSize := n / ng // 100000 / 1000 = 100
-		if batchSize == 0 {
-			batchSize = n
-		}
-		for n > 0 {
-			wg.Add(1)
-			funcCallPerGoroutine := min(n, batchSize) // 100
-			n -= funcCallPerGoroutine                 // 99900
-			go func(quota int) {
-				for atomic.LoadInt64(&trigger) == 0 {
-					runtime.Gosched()
-				}
-				for i := 0; i < quota; i++ {
-					procFunc(int64(i))
-				}
-				wg.Done()
-			}(funcCallPerGoroutine)
-		}
+const (
+	_ = 1 << (10 * iota)
+	_
+	MiB
+)
 
-		b.StartTimer()
-		atomic.StoreInt64(&trigger, 1)
-		wg.Wait()
-		b.StopTimer()
-	})
-}
-
-// runnerParallel - run batched goroutines
-func runnerParallel(b *testing.B, name string, ng int, funcWithGo func(i int64), procWg *sync.WaitGroup) bool {
-	return b.Run(fmt.Sprintf("type:%s-goroutines:%d", name, ng), func(b *testing.B) {
-		var wg sync.WaitGroup
-		var trigger int64 = 0
-		n := b.N
-		// if we will get batchSize = 1000 and n = 100k
-		// we will start 1000 goroutines, each of which will start 100 goroutines
-		batchSize := n / ng // 100000 / 1000 = 100
-		if batchSize == 0 {
-			batchSize = n
-		}
-		for n > 0 {
-			wg.Add(1)
-			goCallsPerGoroutine := min(n, batchSize) // 100
-			n -= goCallsPerGoroutine                 // 99900
-			go func(quota int) {
-				for atomic.LoadInt64(&trigger) == 0 {
-					runtime.Gosched()
-				}
-				for i := 0; i < quota; i++ {
-					funcWithGo(int64(i))
-				}
-				wg.Done()
-			}(goCallsPerGoroutine)
-		}
-
-		b.StartTimer()
-		atomic.StoreInt64(&trigger, 1)
-		wg.Wait()
-		procWg.Wait()
-		b.StopTimer()
-	})
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
+type (
+	Foo struct {
+		i int
 	}
-	return b
+	Bar struct {
+		i int
+	}
+)
+
+type (
+	StackGenerics[T any] []T
+	StackInterface       []interface{}
+	StackFoo             []Foo
+)
+
+////// Methods for StackGenerics type //////////////
+func (s StackGenerics[T]) Peek() T {
+	return s[len(s)-1]
+}
+
+func (s *StackGenerics[T]) Pop() {
+	*s = (*s)[:len(*s)-1]
+}
+
+func (s *StackGenerics[T]) Push(value T) {
+	*s = append(*s, value)
+}
+
+////// Methods for StackInterface type //////////////
+func (s StackInterface) Peek() interface{} {
+	return s[len(s)-1]
+}
+
+func (s *StackInterface) Pop() {
+	*s = (*s)[:len(*s)-1]
+}
+
+func (s *StackInterface) Push(value interface{}) {
+	*s = append(*s, value)
+}
+
+////// Methods for StackFoo type //////////////
+func (s StackFoo) Peek() Foo {
+	return s[len(s)-1]
+}
+
+func (s *StackFoo) Pop() {
+	*s = (*s)[:len(*s)-1]
+}
+
+func (s *StackFoo) Push(value Foo) {
+	*s = append(*s, value)
+}
+
+////// Global vars for disable compiler optimizations for loops //////
+var (
+	foo Foo
+	bar Bar
+)
+
+func BenchmarkStackInterface(b *testing.B) {
+	var s StackInterface
+	for i := 0; i < b.N; i++ {
+		s.Push(Foo{})
+		s.Push(Foo{})
+		s.Pop()
+		foo = s.Peek().(Foo) // type assertion that cannot be optimized as unusable var, so we no need to use sink var
+	}
+
+	b.StopTimer()
+	stats := checkMem()
+	b.Logf("memory usage:%d MB", stats.TotalAlloc/MiB)
+	b.Logf("GC cycles: %d", stats.NumGC)
+}
+
+func BenchmarkStackGenericsFoo(b *testing.B) {
+	var s StackGenerics[Foo]
+	for i := 0; i < b.N; i++ {
+		s.Push(Foo{})
+		s.Push(Foo{})
+		s.Pop()
+		foo = s.Peek()
+	}
+
+	b.StopTimer()
+	stats := checkMem()
+	b.Logf("memory usage:%d MB", stats.TotalAlloc/MiB)
+	b.Logf("GC cycles: %d", stats.NumGC)
+}
+
+func BenchmarkStackGenericsBar(b *testing.B) {
+	var s StackGenerics[Bar]
+	for i := 0; i < b.N; i++ {
+		s.Push(Bar{})
+		s.Push(Bar{})
+		s.Pop()
+		bar = s.Peek()
+	}
+
+	b.StopTimer()
+	stats := checkMem()
+	b.Logf("memory usage:%d MB", stats.TotalAlloc/MiB)
+	b.Logf("GC cycles: %d", stats.NumGC)
+}
+
+func BenchmarkStackTyped(b *testing.B) {
+	var s StackFoo
+	for i := 0; i < b.N; i++ {
+		s.Push(Foo{})
+		s.Push(Foo{})
+		s.Pop()
+		foo = s.Peek()
+	}
+
+	b.StopTimer()
+	stats := checkMem()
+	b.Logf("memory usage:%d MB", stats.TotalAlloc/MiB)
+	b.Logf("GC cycles: %d", stats.NumGC)
 }
 
 func checkMem() *runtime.MemStats {
