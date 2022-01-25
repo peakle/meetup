07936cdf06ffc9d92d3d3a508c948d6c07502e9a
Denis <denislimarev@gmail.com>
2022-01-25 13:54:59 +0300

    add test code

--- go-fuzzing-testing/fuzzing_test.go
+++ go-fuzzing-testing/fuzzing_test.go
@@ -1,1 +1,23 @@
 package fuzzing
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc)  // Use f.Add to provide a seed corpus
+	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse(orig)
+		doubleRev := Reverse(rev)
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
+}
